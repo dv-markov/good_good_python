83ab829d5a55dc995193002deef492b7f8e21ff4
dv-markov <dmitry.v.markov@gmail.com>
2022-06-01 22:00:32 +0300

    Stepik Kind Old Python Course

--- 9 Generators/ex9_3_Map.py
+++ 9 Generators/ex9_3_Map.py
@@ -53,9 +53,9 @@ b = map(lambda s: s[::-1], cities)
 a = list(b)
 print(a)
 
-s = map(int, input().split())
-a = list(s)
-print(a)
+# s = map(int, input().split())
+# a = list(s)
+# print(a)
 
 # Task 1
 # m = map(float, input().split())
@@ -66,3 +66,48 @@ print(a)
 # Task 2
 # lst = list(map(lambda x: abs(int(x)), input().split()))
 # print(*lst)
+
+# Task 3
+# import sys
+#
+# # считывание списка из входного потока
+# lst_in = list(map(str.strip, sys.stdin.readlines()))
+#
+# # здесь продолжайте программу (используйте список lst_in)
+# # переменную lst_in не менять!
+# print(lst_in)
+# lst2D = [list(map(int, x.split())) for x in lst_in]
+# print(lst2D)
+
+# Task 4
+# ввод строки (переменную s не менять)
+# s = input()
+s = 'house=дом car=машина men=человек tree=дерево'
+s_lst = s.split()
+# здесь продолжайте программу
+print(s_lst)
+[print(x) for x in s_lst]
+tp = tuple(tuple(x.split('=')) for x in s_lst)
+print(tp)
+
+# Variant 2
+# s = input()
+s_lst = s.split()
+tp = tuple(map(lambda x: tuple(x.split('=')), s_lst))
+print(tp)
+
+# Task 5
+t = {'ё': 'yo', 'а': 'a', 'б': 'b', 'в': 'v', 'г': 'g', 'д': 'd', 'е': 'e', 'ж': 'zh',
+     'з': 'z', 'и': 'i', 'й': 'y', 'к': 'k', 'л': 'l', 'м': 'm', 'н': 'n', 'о': 'o', 'п': 'p',
+     'р': 'r', 'с': 's', 'т': 't', 'у': 'u', 'ф': 'f', 'х': 'h', 'ц': 'c', 'ч': 'ch', 'ш': 'sh',
+     'щ': 'shch', 'ъ': '', 'ы': 'y', 'ь': '', 'э': 'e', 'ю': 'yu', 'я': 'ya'}
+# s = input()
+s = 'Привет Питон'
+res = map(lambda x: t.get(x, '-'), s.lower())
+print(''.join(res))
+
+# Task 6
+s = 'Москва Уфа Вологда Тула Владивосток Хабаровск'
+# s = input()
+lst = map(lambda x: x if len(x) > 5 else '-', s.split())
+print(*lst)

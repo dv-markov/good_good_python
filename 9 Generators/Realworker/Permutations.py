e1aacb92e71f0d1d19e682806cccb06d8fc4f0c5
dv-markov <dmitry.v.markov@gmail.com>
2022-06-04 18:51:16 +0300

    Stepik Kind Old Python Course

--- 9 Generators/Realworker/Permutations.py
+++ 9 Generators/Realworker/Permutations.py
@@ -15,3 +15,17 @@ lst1 = get_permutations(lst=lst0)
 print(len(lst1))
 for k in lst1:
     print(k)
+
+R = 8
+lst2 = get_permutations(lst=list(range(R)))
+print(len(lst2))
+
+try:
+    with open('perm_result_2.txt', 'w') as file:
+        file.write("Перестановки массива: " + str(list(range(R))) + '\n')
+        file.write("Количество перестановок: " + str(len(lst2)) + '\n')
+        for x in lst2:
+            file.write(str(x) + '\n')
+        print("Файл успешно записан")
+except:
+    print("Ошибка при работе с файлом")
